5b7424e6a54365659c6e8fe38c1a2717a26cc6ee
vcky4 <okon.victor.u@gmail.com>
2024-09-17 15:54:40 +0100

    fix: some fixes

--- apps/auth-microservice/src/user/user.service.ts
+++ apps/auth-microservice/src/user/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
     return this.prisma.user.findUnique({ where: { email } });
   }
 
-//   async createUser(data: Prisma.UserCreateInput) {
-//     return this.prisma.user.create({ data });
-//   }
+  async createUser(data: any) {
+    return this.prisma.user.create({ data });
+  }
 }
